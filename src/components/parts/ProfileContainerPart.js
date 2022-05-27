370aa39acc8ddc35f12c16443a2f3680fe9cdf91
Masud karim <msmasud578@gmail.com>
2022-05-27 14:35:51 +0600

    Fixing the profile-container position issue and also eiditing the path 1.svg file

--- src/components/parts/ProfileContainerPart.js
+++ src/components/parts/ProfileContainerPart.js
@@ -12,22 +12,24 @@ import "./ProfileContainerPart.scss";
 const ProfileContainerPart = ()=> {
     return (
         <div className= "profile-container">
-                        <img src = {BackgroundSvg} alt = "something went wrong" className = "profile-container__profile-img-background"/>
-                        <div className = "profile-container__profile-image-name-bundle">
-                            <div>
-                                <h1>Md Masud karim</h1>
-                                <span style = {{display:"flex",gap:"15px",float:"right",alignItems:"center"}}>
-                                    <img src = {iconCoffe} alt = "something went wrong" className= "profile-container__profile-image-name-bundle__coffe-icon"/>
-                                    <p>Software engineer</p>
-                                </span>
+                       <div>
+                            <img src = {BackgroundSvg} alt = "something went wrong" className = "profile-container__profile-img-background"/>
+                            <div className = "profile-container__profile-image-name-bundle">
+                                <div>
+                                    <h1>Md Masud karim</h1>
+                                    <span style = {{display:"flex",gap:"15px",float:"right",alignItems:"center"}}>
+                                        <img src = {iconCoffe} alt = "something went wrong" className= "profile-container__profile-image-name-bundle__coffe-icon"/>
+                                        <p>Software engineer</p>
+                                    </span>
+                                </div>
+
+                                <img src = {profileImg} alt = "something went wrong" className = "profile-container__profile-image-name-bundle__profile-image"/>
+                                
+                                
+                                <button className = "profile-container__profile-image-name-bundle__hireme-btn"><img className = "profile-container__profile-image-name-bundle__upwork-icon" src ={upworkIcon}  alt = "something went wrong"/>Hire Me</button>
+
                             </div>
-
-                            <img src = {profileImg} alt = "something went wrong" className = "profile-container__profile-image-name-bundle__profile-image"/>
-                            
-                            
-                            <button className = "profile-container__profile-image-name-bundle__hireme-btn"><img className = "profile-container__profile-image-name-bundle__upwork-icon" src ={upworkIcon}  alt = "something went wrong"/>Hire Me</button>
-
-                        </div>
+                       </div>
 
                     </div>
     );
